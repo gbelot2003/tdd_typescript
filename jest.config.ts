960dcd5ec92113cc64d88b4d4dfc3acf51bd931c
Gerardo Belot <gerardo.belot@gmail.com>
2023-02-24 20:01:54 -0600

    configurando entorno de pruebas y exclusion de folders no necesarios

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    //testEnvironment: nodejs,
+    testMatch: ["**/**/*.test.ts"],
+    verbose: true,
+    forceExit: true,
+    //clearMocks: true
+    modulePathIgnorePatterns: ["<rootDir>/db", "<rootDir>/node_modules", "<rootDir>/dockerfiles"]
+
+}
+
+export default config;
+
---
+++ src/__test__/register.test.ts
@@ -0,0 +1,7 @@
+
+describe('register', () => {
+    it('sum 2 number correct', () => {
+        let suma = 1 + 1;
+        expect(suma).toBe(2)
+    })
+})
