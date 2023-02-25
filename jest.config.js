c4aad63159dc0074dfeaf12bfe1adce5e9eee05f
Administrator <admin@example.com>
2023-02-24 19:40:56 -0600

    agregando configuraciones para jest.conf

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.export = {
     preset: 'ts-jest',
-    testEnvironment: node
+    testEnvironment: node,
+    testMatch: ["**/**/*.test.ts"],
+    verbose: true,
+    forceExit: true,
+    //clearMocks: true
 }
--- src/connections/Connec.connections.ts
+++ src/connections/Connec.connections.ts
@@ -2,4 +2,5 @@ import { PostgresConn } from './Postgres.connection'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const sqlconn = new PostgresConn(process.env.DB_DATABASE || 'node_db', process.env.DB_USER || 'root', process.env.DB_ROOT_PASSWORD || 'password', process.env.DB_HOST || 'postgres')
+export const sqlconn = new PostgresConn(process.env.DB_DATABASE || 'node_db', 
+    process.env.DB_USER || 'root', process.env.DB_ROOT_PASSWORD || 'password', process.env.DB_HOST || 'postgres')
