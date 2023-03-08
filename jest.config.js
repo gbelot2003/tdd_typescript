42d015b38f8bf4cb5306a16f230588d57645d8a3
Gerardo Belot <gerardo.belot@gmail.com>
2023-03-08 00:54:23 -0600

    Configurando jest TS he importando @types/jest

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.export = {
-    preset: 'ts-jest',
-    testEnvironment: node
-}
--- src/config/sqlite.connect.ts
+++ src/config/sqlite.connect.ts
@@ -1,6 +1,14 @@
 import { Sqlite } from '../connections/Sqlite.connections'
+import { PostgresConn } from '../connections/Postgres.connection'
 import dotenv from 'dotenv'
 dotenv.config()
 
-   // ahora la coneccion se guarda aqui 
+// ahora la coneccion se guarda aqui 
 export const sequelize = new Sqlite().createConnection()
+
+/** solo tenemos que cambiar la conexion para
+ *  mudar la base de datos                 
+ *******************************************/
+//export const sequelize = new PostgresConn(process.env.DB_DATABASE || 
+//      'node', process.env.DB_USER || 'root', process.env.DB_ROOT_PASSWORD || 
+//         'pass', process.env.DB_HOST || 'mysql').createConnection
